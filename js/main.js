421ae4aa838aa3b6c37e3b41ea836afac662953e
VictoriaKonuhova <konyukhova.victoria@gmail.com>
2022-10-29 00:33:22 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 1000) {
+      $(".header__top").addClass("fixed");
+    } else {
+      $(".header__top").removeClass("fixed");
+    }
+  });
+
+  $(".menu a, .footer__logo").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+  
+  $('.slider-blog__inner').slick({
+    dots: true,
+    arrows: false,
+  });
+
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active')
+  });
+  
+  var mixer = mixitup('.gallery__content');
+});
